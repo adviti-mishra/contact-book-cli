0cd0e80b2b00075b00344604d66c35f56887d8c6
Adviti Mishra <adviti@umich.edu>
2024-05-10 18:16:00 +0600

    Create a contact with specific attributes

---
+++ cb_ruby_driver.rb
@@ -0,0 +1,8 @@
+class Contact
+    def initialize(name, email_address, phone_number, street_address)
+        @contact_name = name
+        @contact_email_address = email 
+        @contact_phone_number = phone_number
+        @contact_street_address = street_address 
+    end 
+end 
