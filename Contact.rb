33cc63e5c6ed6acf690e6fd60c08ba0a804a64a6
Adviti Mishra <adviti@umich.edu>
2024-05-10 18:23:29 +0600

    Defined interface for a Contact class in a separate file to be imported in both driver and odm implementations. Added requirements on README

--- Contact.rb
+++ Contact.rb
@@ -4,5 +4,22 @@ class Contact
         @contact_email_address = email 
         @contact_phone_number = phone_number
         @contact_street_address = street_address 
+        create_contact_in_db
     end 
+
+    def create_contact_in_db
+    end 
+
+    def list_all_contacts
+    end
+
+    def query_by_name
+    end
+
+    def query_by_email
+    end
+
+    def query_by_number
+    end
+
 end 
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # contact-book-cli
+
+## requirements:
+
+Functionalities:
+
+1. Create contact with name, email address, phone number, and street address
+2. List all contacts
+3. Query specific contact by name, email, or phone number
+   Do this with both MongoDB Ruby Driver and Mongoid
