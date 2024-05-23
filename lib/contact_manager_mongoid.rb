01eb2976038a20db4c5cc9ff990bc5cebb56862f
Adviti Mishra <adviti@umich.edu>
2024-05-23 11:33:11 +0530

    Loading configuration manually in initializing Mongoid contact manager

--- lib/contact_manager_mongoid.rb
+++ lib/contact_manager_mongoid.rb
@@ -1,2 +1,8 @@
 require 'mongoid'
 require_relative 'contact'
+
+class ContactManager
+  def initialize
+    # loading configuration manually because this app is not using Ruby on Rails
+    Mongoid.load!('config/mongoid.yml', :development)
+  end
