2f8417de053cfb3a5311ce4b2388207594a42a20
Adviti Mishra <adviti@umich.edu>
2024-05-23 11:56:46 +0530

    Query by email with mongoid

--- lib/contact_manager_mongoid.rb
+++ lib/contact_manager_mongoid.rb
@@ -41,3 +41,14 @@ class ContactManager
       puts JSON.pretty_generate(document)
     end
   end
+
+  # REQUIRES: email_address_in is well-formatted
+  # EFFECTS: Prints all contacts with email_address == email_address_in
+  def query_by_email(email_address_in)
+    # Access all documents in the contacts collection with email_address == email_address_in
+    contacts = Contact.where email_address: email_address_in
+    contacts.each do |document|
+      # print the contact
+      puts JSON.pretty_generate(document)
+    end
+  end
