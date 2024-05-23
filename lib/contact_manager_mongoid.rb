dca904fcd9960463f22dcf5098033d26c2a43d5c
Adviti Mishra <adviti@umich.edu>
2024-05-23 11:35:43 +0530

    RMEs for initialize and creating a contact along with logic of creating a contact using ORM

--- lib/contact_manager_mongoid.rb
+++ lib/contact_manager_mongoid.rb
@@ -2,7 +2,21 @@ require 'mongoid'
 require_relative 'contact'
 
 class ContactManager
+
+  # EFFECTS: loads configuration manually
   def initialize
     # loading configuration manually because this app is not using Ruby on Rails
     Mongoid.load!('config/mongoid.yml', :development)
+  end
+
+  # REQUIRES: name_in, email_address_in, phone_number_in, street_address_in are well-formatted
+  # MODIFIES: the contacts collection
+  # EFFECTS: Creates a new contact
+  def create_contact_in_db(name_in ,email_address_in, phone_number_in, street_address_in)
+    Contact.create(
+      name: name_in, 
+      email_address: email_address_in,
+      phone_number: phone_number_in, 
+      street_address: street_address_in
+    )
   end
